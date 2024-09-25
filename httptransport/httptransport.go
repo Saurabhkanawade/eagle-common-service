11ac757bb6f2d42899b8c20706494ac4469c4f62
skanawade <skanawade@greenlighthealth.com>
2024-09-25 12:54:22 +0530

    resoleve the code

--- httptransport/httptransport.go
+++ httptransport/httptransport.go
@@ -12,7 +12,7 @@ func EncodeResponse(ctx context.Context, w http.ResponseWriter, i interface{}) e
 	return json.NewEncoder(w).Encode(i)
 }
 
-func EncodePostResponse(ctx context.Context, w http.ResponseWriter, r http.Request, i interface{}) error {
+func EncodePostResponse(ctx context.Context, w http.ResponseWriter, i interface{}) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	return json.NewEncoder(w).Encode(i)
